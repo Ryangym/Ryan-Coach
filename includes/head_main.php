4f2dc1f4bf76b1a773c0d3451b6738f967453b6b
ryan <ryano0661@gmail.com>
2025-12-09 16:46:57 -0300

    app teste

--- includes/head_main.php
+++ includes/head_main.php
@@ -15,3 +15,21 @@
 <link href="https://fonts.googleapis.com/css2?family=Cherry+Cream+Soda&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Ms+Madi&family=Orbitron:wght@700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 <!-- _____________________________ FIM FONTES____________________________________ -->
+
+<!-- _____________________________ APP ____________________________________ -->
+<link rel="manifest" href="manifest.json">
+<meta name="theme-color" content="#FFBA42"> <meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="apple-mobile-web-app-title" content="Ryan Coach">
+<link rel="apple-touch-icon" href="assets/img/icones/favicon.png">
+
+<script>
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('service-worker.js')
+                .then(reg => console.log('App pronto! Service Worker registrado.', reg))
+                .catch(err => console.log('Erro ao registrar App:', err));
+        });
+    }
+</script>
+<!-- _____________________________ FIM APP ____________________________________ -->
